14c5b9cf314f459729816435e9f2d91d9e4b44df
fossilkeeper24 <babuli.rainbows@gmail.com>
2025-11-11 22:22:56 +0000

    Cleaned up firmware a lil
    long pending commit but also added some code to help troubleshooting

--- sharfinder - all/sharfinder__code/sharfinder__code.ino
+++ sharfinder - all/sharfinder__code/sharfinder__code.ino
@@ -1,10 +1,10 @@
-int BUT1 = D4;
-int BUT2 = D5;
-int BUT3 = D6;
+int BUT1 = D3;
+int BUT2 = D4;
+int BUT3 = D5;
 
-int LED1 = D1;
-int LED2 = D2;
-int LED3 = D3;
+int LED1 = D0;
+int LED2 = D1;
+int LED3 = D2;
 
 long but1time; // i want to use these elsewhere for minigameee
 long but2time;
@@ -13,13 +13,22 @@ long but3time;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(BUT1, INPUT);
-  pinMode(BUT2, INPUT);
-  pinMode(BUT3, INPUT);
+  Serial.begin(9600);
+  pinMode(BUT1, INPUT_PULLUP);
+  pinMode(BUT2, INPUT_PULLUP);
+  pinMode(BUT3, INPUT_PULLUP);
 
-  pinMode(LED1, OUTPUT);
+//  pinMode(LED1, OUTPUT);
+  pinMode(0, OUTPUT);
   pinMode(LED2, OUTPUT);
   pinMode(LED3, OUTPUT);
+
+  pinMode(LED_BUILTIN, OUTPUT); //desperately testing
+
+  digitalWrite(LED1, LOW); //desperately testing
+  delay(100);
+  digitalWrite(LED1, HIGH);
+  delay(100);
 
   long startTime = millis();
 
@@ -27,40 +36,31 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  
-  if (digitalRead(BUT1) == HIGH) {
-    but1time = millis();
-    digitalWrite(LED1, HIGH);
-  } else {
-    digitalWrite(LED1, LOW);
-  }
+  //flash_userled();
+  int status1 = digitalRead(BUT1);
+  int status2 = digitalRead(BUT2);
+  int status3 = digitalRead(BUT3);
 
-  if (digitalRead(BUT2) == HIGH) {
-    but2time = millis();
-    digitalWrite(LED2, HIGH);
-  } else {
-    digitalWrite(LED2, LOW);
-  }
-
-  if (digitalRead(BUT3) == HIGH) {
-    digitalWrite(LED3, HIGH);
-    but3time = millis();
-  } else {
-    digitalWrite(LED3, LOW);
-    but3time = 0;
-  }
-
-  secret();
-
-  if (but3time - but2time <= 1000){
-    if (but2time - but1time <= 1000){
-      initialiseGame();
+  Serial.println("button1:");
+  Serial.println(status1);
+  Serial.println("button2:");
+  Serial.println(status2);
+  Serial.println("button3:");
+  Serial.println(status3);
+  delay(400);
 
 
 
-    }
-  }
+ //  secret();
+
+  //if (but3time - but2time <= 1000){
+    //if (but2time - but1time <= 1000){
+      //initialiseGame();
+
+
+
+    //}
+  //}
 
 }
 
@@ -98,4 +98,59 @@ void initialiseGame() {
 
     }
 
+void testing() {
+  digitalWrite(LED1, LOW);
+  delay(1000);
+  digitalWrite(LED1, HIGH);
+  delay(1000);
+
+  digitalWrite(LED2, LOW); //desperately testing
+  delay(1000);
+  digitalWrite(LED2, HIGH);
+  delay(1000);
+
+  digitalWrite(LED3, LOW); //desperately testing
+  delay(1000);
+  digitalWrite(LED3, HIGH);
+  delay(1000);
+
+
+}
+
+void flash_userled() {
+  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(1000);                      // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+  delay(1000);                      // wait for a second
+
+}
+
+void real() {
+  if (digitalRead(BUT1) == LOW) {
+    but1time = millis();
+    digitalWrite(LED1, HIGH);
+    Serial.println("But1 pressed");
+  } else {
+    digitalWrite(LED1, LOW);
+  }
+
+  if (digitalRead(BUT2) == LOW) {
+    but2time = millis();
+    digitalWrite(LED2, HIGH);
+    Serial.println("But2 pressed");
+  } else {
+    digitalWrite(LED2, LOW);
+  }
+
+  if (digitalRead(BUT3) == LOW) {
+    digitalWrite(LED3, HIGH);
+    Serial.println("But3 pressed");
+    but3time = millis();
+  } else {
+    digitalWrite(LED3, LOW);
+    but3time = 0;
+  }
+
+
+}
 
